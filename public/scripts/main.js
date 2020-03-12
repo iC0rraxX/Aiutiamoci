dad17e2c8690545e39e02f0078d9b977f098e3b7
Corrado Verhoeven <corrado.verhoeven@gmail.com>
2020-03-12 12:42:48 +0100

    Initial release

---
+++ public/scripts/main.js
@@ -0,0 +1,480 @@
+/**
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+// Shortcuts to DOM Elements.
+var recapitoInput = document.getElementById('recapito');
+var messageForm = document.getElementById('message-form');
+var messageInput = document.getElementById('new-post-message');
+var titleInput = document.getElementById('new-post-title');
+var signInButton = document.getElementById('sign-in-button');
+// var signInButtonTel = document.getElementById('sign-in-button-phone');
+var signInButtonFB = document.getElementById('sign-in-button-fb');
+var signOutButton = document.getElementById('sign-out-button');
+var splashPage = document.getElementById('page-splash');
+var addPost = document.getElementById('add-post');
+var addButton = document.getElementById('add');
+var recentPostsSection = document.getElementById('recent-posts-list');
+var userPostsSection = document.getElementById('user-posts-list');
+var topUserPostsSection = document.getElementById('top-user-posts-list');
+var recentMenuButton = document.getElementById('menu-recent');
+var myPostsMenuButton = document.getElementById('menu-my-posts');
+var myTopPostsMenuButton = document.getElementById('menu-my-top-posts');
+var listeningFirebaseRefs = [];
+
+/**
+ * Saves a new post to the Firebase DB.
+ */
+// [START write_fan_out]
+function writeNewPost(uid, username, picture, title, body, tel) {
+  // A post entry.
+  var postData = {
+    author: username,
+    uid: uid,
+    body: body,
+    tel: tel,
+    title: title,
+   // starCount: 0,
+    authorPic: picture
+  };
+
+  // Get a key for a new Post.
+  var newPostKey = firebase.database().ref().child('posts').push().key;
+
+  // Write the new post's data simultaneously in the posts list and the user's post list.
+  var updates = {};
+  updates['/posts/' + newPostKey] = postData;
+  updates['/user-posts/' + uid + '/' + newPostKey] = postData;
+
+  return firebase.database().ref().update(updates);
+}
+// [END write_fan_out]
+
+/**
+ * Creates a post element.
+ */
+function createPostElement(postId, title, text, tel, author, authorId, authorPic) {
+  var uid = firebase.auth().currentUser.uid;
+
+  var html =
+  	'<div class="card mdl-card">' +
+          '<div class="card-body mdl-card__title">' +
+            '<h4 class="title card-text"></h4>' +
+            '<p><img class="avatar" style="height:32px;width:32px;background-size:32px 32px;border-radius:32px;border: 2px white solid;margin-right:10px;"><span class="username card-subtitle mb-2 text-muted"></span></p>' +
+            '<div class="card-text">☎️: <span class="tel card-text"></span></div>' +
+			'<div class="text card-text"></div>' + 
+		  '</div>' +
+		  '<div class="card-footer text-muted">' +
+		  	'<div class="comments-container"></div>' +
+		  	'<form class="add-comment" action="#">' +
+           	 '<div class="form-group mdl-textfield mdl-js-textfield">' +
+             	 '<input placeholder="Commenta..." aria-label="Commenta..." class="form-control input-sm new-comment" type="text" required>' +
+              '</div>' +
+			'</form>' +
+		  '</div>' + 
+    '</div>';
+
+  // Create the DOM element from the HTML.
+  var div = document.createElement('div');
+  div.innerHTML = html;
+  var postElement = div.firstChild;
+  if (componentHandler) {
+    componentHandler.upgradeElements(postElement.getElementsByClassName('mdl-textfield')[0]);
+  }
+
+  var addCommentForm = postElement.getElementsByClassName('add-comment')[0];
+  var commentInput = postElement.getElementsByClassName('new-comment')[0];
+
+
+  // Set values.
+  postElement.getElementsByClassName('text')[0].innerText = text;
+  postElement.getElementsByClassName('tel')[0].innerText = tel;
+  postElement.getElementsByClassName('title')[0].innerText = title;
+  postElement.getElementsByClassName('username')[0].innerText = author || 'Anonymous';
+  postElement.getElementsByClassName('avatar')[0].src = (authorPic || './img/silhouette.jpg');
+  
+/*
+  postElement.getElementsByClassName('avatar')[0].style.backgroundImage = 'url("' +
+      (authorPic || './img/silhouette.jpg') + '")';
+*/
+
+  // Listen for comments.
+  // [START child_event_listener_recycler]
+  var commentsRef = firebase.database().ref('post-comments/' + postId);
+  commentsRef.on('child_added', function(data) {
+    addCommentElement(postElement, data.key, data.val().text, data.val().author);
+  });
+
+  commentsRef.on('child_changed', function(data) {
+    setCommentValues(postElement, data.key, data.val().text, data.val().author);
+  });
+
+  commentsRef.on('child_removed', function(data) {
+    deleteComment(postElement, data.key);
+  });
+  // [END child_event_listener_recycler]
+
+
+  // Keep track of all Firebase reference on which we are listening.
+  listeningFirebaseRefs.push(commentsRef);
+
+
+  // Create new comment.
+  addCommentForm.onsubmit = function(e) {
+    e.preventDefault();
+    createNewComment(postId, firebase.auth().currentUser.displayName, uid, commentInput.value);
+    commentInput.value = '';
+    commentInput.parentElement.MaterialTextfield.boundUpdateClassesHandler();
+  };
+
+  return postElement;
+}
+
+/**
+ * Writes a new comment for the given post.
+ */
+function createNewComment(postId, username, uid, text) {
+  firebase.database().ref('post-comments/' + postId).push({
+    text: text,
+    author: username,
+    uid: uid
+  });
+}
+
+/**
+ * Updates the starred status of the post.
+ */
+function updateStarredByCurrentUser(postElement, starred) {
+  if (starred) {
+    postElement.getElementsByClassName('starred')[0].style.display = 'inline-block';
+    postElement.getElementsByClassName('not-starred')[0].style.display = 'none';
+  } else {
+    postElement.getElementsByClassName('starred')[0].style.display = 'none';
+    postElement.getElementsByClassName('not-starred')[0].style.display = 'inline-block';
+  }
+}
+
+
+/**
+ * Creates a comment element and adds it to the given postElement.
+ */
+function addCommentElement(postElement, id, text, author) {
+  var comment = document.createElement('div');
+  comment.classList.add('comment-' + id);
+  comment.innerHTML = '<p><span class="username" style="font-weight:bold"></span>: <span class="comment"></span></p>';
+  comment.getElementsByClassName('comment')[0].innerText = text;
+  comment.getElementsByClassName('username')[0].innerText = author || 'Anonymous';
+
+  var commentsContainer = postElement.getElementsByClassName('comments-container')[0];
+  commentsContainer.appendChild(comment);
+}
+
+/**
+ * Sets the comment's values in the given postElement.
+ */
+function setCommentValues(postElement, id, text, author) {
+  var comment = postElement.getElementsByClassName('comment-' + id)[0];
+  comment.getElementsByClassName('comment')[0].innerText = text;
+  comment.getElementsByClassName('fp-username')[0].innerText = author;
+}
+
+/**
+ * Deletes the comment of the given ID in the given postElement.
+ */
+function deleteComment(postElement, id) {
+  var comment = postElement.getElementsByClassName('comment-' + id)[0];
+  comment.parentElement.removeChild(comment);
+}
+
+/**
+ * Starts listening for new posts and populates posts lists.
+ */
+function startDatabaseQueries() {
+  // [START my_top_posts_query]
+  var myUserId = firebase.auth().currentUser.uid;
+  var topUserPostsRef = firebase.database().ref('user-posts/' + myUserId).orderByChild('starCount');
+  // [END my_top_posts_query]
+  // [START recent_posts_query]
+  var recentPostsRef = firebase.database().ref('posts').limitToLast(100);
+  // [END recent_posts_query]
+  var userPostsRef = firebase.database().ref('user-posts/' + myUserId);
+
+  var fetchPosts = function(postsRef, sectionElement) {
+    postsRef.on('child_added', function(data) {
+      var author = data.val().author || 'Anonymous';
+      var containerElement = sectionElement.getElementsByClassName('posts-container')[0];
+      containerElement.insertBefore(
+        createPostElement(data.key, data.val().title, data.val().body, data.val().tel, author, data.val().uid, data.val().authorPic),
+        containerElement.firstChild);
+    });
+    postsRef.on('child_changed', function(data) {
+      var containerElement = sectionElement.getElementsByClassName('posts-container')[0];
+      var postElement = containerElement.getElementsByClassName('post-' + data.key)[0];
+      postElement.getElementsByClassName('title')[0].innerText = data.val().title;
+      postElement.getElementsByClassName('username')[0].innerText = data.val().author;
+      postElement.getElementsByClassName('text')[0].innerText = data.val().body;
+      // postElement.getElementsByClassName('star-count')[0].innerText = data.val().starCount;
+    });
+    postsRef.on('child_removed', function(data) {
+      var containerElement = sectionElement.getElementsByClassName('posts-container')[0];
+      var post = containerElement.getElementsByClassName('post-' + data.key)[0];
+      post.parentElement.removeChild(post);
+    });
+  };
+
+  // Fetching and displaying all posts of each sections.
+  fetchPosts(topUserPostsRef, topUserPostsSection);
+  fetchPosts(recentPostsRef, recentPostsSection);
+  fetchPosts(userPostsRef, userPostsSection);
+
+  // Keep track of all Firebase refs we are listening to.
+  listeningFirebaseRefs.push(topUserPostsRef);
+  listeningFirebaseRefs.push(recentPostsRef);
+  listeningFirebaseRefs.push(userPostsRef);
+}
+
+/**
+ * Writes the user's data to the database.
+ */
+// [START basic_write]
+function writeUserData(userId, name, email, imageUrl) {
+  firebase.database().ref('users/' + userId).set({
+    username: name,
+    email: email,
+    profile_picture : imageUrl
+  });
+}
+// [END basic_write]
+
+/**
+ * Cleanups the UI and removes all Firebase listeners.
+ */
+function cleanupUi() {
+  // Remove all previously displayed posts.
+  topUserPostsSection.getElementsByClassName('posts-container')[0].innerHTML = '';
+  recentPostsSection.getElementsByClassName('posts-container')[0].innerHTML = '';
+  userPostsSection.getElementsByClassName('posts-container')[0].innerHTML = '';
+
+  // Stop all currently listening Firebase listeners.
+  listeningFirebaseRefs.forEach(function(ref) {
+    ref.off();
+  });
+  listeningFirebaseRefs = [];
+}
+
+/**
+ * The ID of the currently signed-in User. We keep track of this to detect Auth state change events that are just
+ * programmatic token refresh but not a User status change.
+ */
+var currentUID;
+
+/**
+ * Triggers every time there is a change in the Firebase auth state (i.e. user signed-in or user signed out).
+ */
+function onAuthStateChanged(user) {
+  // We ignore token refresh events.
+  if (user && currentUID === user.uid) {
+    return;
+  }
+
+  cleanupUi();
+  if (user) {
+    currentUID = user.uid;
+    splashPage.style.display = 'none';
+    addButton.style.display = '';
+    signOutButton.style.display = '';
+    writeUserData(user.uid, user.displayName, user.email, user.photoURL);
+    $("#welcomeModal").modal()
+    startDatabaseQueries();
+  } else {
+    // Set currentUID to null.
+    currentUID = null;
+    // Display the splash page where you can sign-in.
+    splashPage.style.display = '';
+    addButton.style.display = 'none';
+    signOutButton.style.display = 'none';
+  }
+}
+
+/**
+ * Creates a new post for the current user.
+ */
+function newPostForCurrentUser(title, text, tel) {
+  // [START single_value_read]
+  var userId = firebase.auth().currentUser.uid;
+  return firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
+    var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
+    // [START_EXCLUDE]
+    return writeNewPost(firebase.auth().currentUser.uid, username,
+      firebase.auth().currentUser.photoURL,
+      title, text, tel);
+    // [END_EXCLUDE]
+  });
+  // [END single_value_read]
+}
+
+/**
+ * Displays the given section element and changes styling of the given button.
+ */
+function showSection(sectionElement, buttonElement) {
+  recentPostsSection.style.display = 'none';
+  userPostsSection.style.display = 'none';
+  topUserPostsSection.style.display = 'none';
+  addPost.style.display = 'none';
+  recentMenuButton.classList.remove('is-active');
+  myPostsMenuButton.classList.remove('is-active');
+  
+//   myTopPostsMenuButton.classList.remove('is-active');
+
+  if (sectionElement) {
+    sectionElement.style.display = 'block';
+  }
+  if (buttonElement) {
+    buttonElement.classList.add('is-active');
+  }
+}
+
+// Bindings on load.
+window.addEventListener('load', function() {
+  // Bind Sign in button.
+  signInButton.addEventListener('click', function() {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider);
+  });
+  
+  
+  signInButtonFB.addEventListener('click', function() {
+    var provider2 = new firebase.auth.FacebookAuthProvider();
+    provider2.addScope('email');
+    firebase.auth().useDeviceLanguage();
+	firebase.auth().signInWithPopup(provider2).then(function(result) {
+	  // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+	  var token = result.credential.accessToken;
+	  // The signed-in user info.
+	  var user = result.user;
+	 
+	  
+	}).catch(function(error) {
+	  // Handle Errors here.
+	  var errorCode = error.code;
+	  var errorMessage = error.message;
+	  // The email of the user's account used.
+	  var email = error.email;
+	  // The firebase.auth.AuthCredential type that was used.
+	  var credential = error.credential;
+	  // ...
+	});
+  });
+  
+  
+  
+  // Bind Sign out button.
+  signOutButton.addEventListener('click', function() {
+    firebase.auth().signOut();
+  });
+
+  // Listen for auth state changes
+  firebase.auth().onAuthStateChanged(onAuthStateChanged);
+
+  // Saves message on form submit.
+  messageForm.onsubmit = function(e) {
+    e.preventDefault();
+    var text = messageInput.value;
+    var tel = recapitoInput.value;
+    var title = titleInput.value;
+    if (text && title) {
+      newPostForCurrentUser(title, text, tel).then(function() {
+        myPostsMenuButton.click();
+      });
+      messageInput.value = '';
+      recapitoInput.value = '';
+      titleInput.value = '';
+    }
+  };
+
+  // Bind menu buttons.
+  recentMenuButton.onclick = function() {
+    showSection(recentPostsSection, recentMenuButton);
+  };
+  myPostsMenuButton.onclick = function() {
+    showSection(userPostsSection, myPostsMenuButton);
+  };
+
+
+  addButton.onclick = function() {
+    showSection(addPost);
+    messageInput.value = '';
+    recapitoInput.value = '';
+    titleInput.value = '';
+  };
+  recentMenuButton.onclick();
+}, false);
+
+
+
+
+// Phone auth
+
+
+/*
+window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier(
+    "recaptcha-container",
+   {
+    size: "normal",
+    callback: function(response) {
+        submitPhoneNumberAuth();
+    }
+   }
+);
+
+
+
+// This function runs when the 'sign-in-button' is clicked
+// Takes the value from the 'phoneNumber' input and sends SMS to that phone number
+function submitPhoneNumberAuth() {
+    // We are using the test phone numbers we created before
+    var phoneNumber = document.getElementById("phoneNumber").value;
+    console.log(phoneNumber);
+    var appVerifier = window.recaptchaVerifier;
+    firebase
+    .auth()
+    .signInWithPhoneNumber(phoneNumber, appVerifier)
+    .then(function(confirmationResult) {
+        window.confirmationResult = confirmationResult;
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
+}
+
+// This function runs when the 'confirm-code' button is clicked
+// Takes the value from the 'code' input and submits the code to verify the phone number
+// Return a user object if the authentication was successful, and auth is complete
+function submitPhoneNumberAuthCode() {
+    // We are using the test code we created before
+    var code = document.getElementById("code").value;
+    console.log(code);
+    confirmationResult
+    .confirm(code)
+    .then(function(result) {
+        var user = result.user;
+        console.log(user);
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
+}
+*/
